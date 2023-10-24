9b3d316ffa45fa6ddd39e435444541e80201eed0
MetroOJT <kouhei-eguchi@metrobooks.co.jp>
2023-10-24 12:35:26 +0900

    リファクタリング

--- API/GetContent/Index.aspx.vb
+++ API/GetContent/Index.aspx.vb
@@ -4,21 +4,34 @@ Imports System.Net
 
 Partial Class API_GetContent_Index
     Inherits System.Web.UI.Page
+
+    'LINEに送るJSONオブジェクトのためのクラス
     Public Class Requestmessage
         Public Property replyToken As String
         Public Property messages As New List(Of Object)
-        Public Sub add_message(obj As Object)
-            messages.Add(obj)
+        Public Sub add_message(str As String)
+            Dim message As Messages = New Messages
+            message.type = "text"
+            message.text = str
+            messages.Add(message)
         End Sub
     End Class
+
+    'Requestmessage.messagesに追加するJSONオブジェクトのクラス
     Public Class Messages
         Public Property type As String
         Public Property text As String
     End Class
+
+    '読み込む前にTLS1.2を適用する
     Public Sub Pre_Load(sender As Object, e As EventArgs) Handles Me.PreLoad
         ServicePointManager.SecurityProtocol = ServicePointManager.SecurityProtocol Or SecurityProtocolType.Tls12
     End Sub
+
+    '読み込み後
     Public Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+        '変数宣言
         Dim cCom As New Common
         Dim cDB As New CommonDB
         Dim Cki As New Cookie
@@ -40,18 +53,30 @@ Partial Class API_GetContent_Index
         Dim ReplyToken As String = ""
         Dim CouponCode As String = ""
         Dim HasEventID_flg As Boolean = False
+
+        'LINE返信用のWebRequest
         Dim req As System.Net.WebRequest =
             System.Net.WebRequest.Create("https://api.line.me/v2/bot/message/reply")
+
+        'Requestmessageのインスタンス化
         Dim requestmessage As Requestmessage = New Requestmessage
+
+        '公式アカウントアクセス用のトークン
         Const AccessToken As String = "p/w9bCbcf1BCehRo4gVCvfz7mG+sCLiUF3AeLm3kzrL8ugjKA0JsRYMx/WDnoqNzQhqLjbFXX9QFn/mBQr5wpC9Nrd7uDVjtBGVLDGqlIsbUh+ycI9zhl1rw/UJE6BUawPXWJZ4VMLRk/ItsQkKA3QdB04t89/1O/w1cDnyilFU="
         Try
+
+            'Requestのデータを読み込む
             len = Request.ContentLength
             bData = Request.BinaryRead(len)
             sPostData = enc.GetString(bData)
             sPostData = HttpUtility.UrlDecode(sPostData)
+
+            'JSONデータに変換
             left = sPostData.IndexOf("{")
             right = sPostData.LastIndexOf("}")
             sjson = sPostData.Substring(left, right - left + 1)
+
+            'Object型に変えて必要なデータを取り出す
             jsonObj = JsonConvert.DeserializeObject(sjson)
             eventsObj = jsonObj("events")(0)
             messageObj = eventsObj("message")
@@ -60,22 +85,24 @@ Partial Class API_GetContent_Index
             Keyword = messageObj("text").ToString()
             ReplyToken = eventsObj("replyToken").ToString()
 
+            '変数をSQLで扱えるようにする
             cDB.AddWithValue("@Line_UserID", Line_UserID)
             cDB.AddWithValue("@Keyword", Keyword)
             cDB.AddWithValue("@ReplyToken", ReplyToken)
             cDB.AddWithValue("@Send", "Send")
-
-            requestmessage.replyToken = ReplyToken
-
-            'Recvlog追加
             cDB.AddWithValue("@Recv", "Recv")
             cDB.AddWithValue("@RecvLog", sjson)
 
+            'ReplyTokenをrequestmessageに設定
+            requestmessage.replyToken = ReplyToken
+
+            'Recvlog追加
             sSQL.Clear()
             sSQL.Append(" INSERT INTO " & cCom.gctbl_LogMst)
-            sSQL.Append(" VALUES(@ReplyToken, @Line_UserID, @Recv, 200, @RecvLog, NOW())")
+            sSQL.Append(" VALUES(@ReplyToken, @Recv, @Line_UserID, 200, @RecvLog, NOW())")
             cDB.ExecuteSQL(sSQL.ToString)
 
+            '送られてきたキーワードが有効か確認
             sSQL.Clear()
             sSQL.Append(" SELECT")
             sSQL.Append("  EventID")
@@ -84,9 +111,13 @@ Partial Class API_GetContent_Index
             sSQL.Append(" Status = 1 AND")
             sSQL.Append(" ScheduleFm <= NOW() And ScheduleTo >= NOW()")
             cDB.SelectSQL(sSQL.ToString)
+
+            'キーワード有効
             If cDB.ReadDr Then
                 HasEventID_flg = True
                 cDB.AddWithValue("@EventID", cDB.DRData("EventID"))
+
+                'キーワードが使用済みか確認
                 sSQL.Clear()
                 sSQL.Append(" SELECT")
                 sSQL.Append("  Line_UserID")
@@ -97,7 +128,10 @@ Partial Class API_GetContent_Index
                 sSQL.Append(" WHERE Line_UserID = @Line_UserID AND Keyword = @Keyword")
                 cDB.SelectSQL(sSQL.ToString)
 
+                'キーワード使用済み
                 If cDB.ReadDr Then
+
+                    '使用済みメッセージを取得
                     sSQL.Clear()
                     sSQL.Append(" SELECT")
                     sSQL.Append("  Message")
@@ -107,54 +141,72 @@ Partial Class API_GetContent_Index
                     sSQL.Append(" WHERE " & cCom.gctbl_EventMst & ".EventID = 0 AND MessageID = 2")
                     cDB.SelectSQL(sSQL.ToString)
                     If cDB.ReadDr Then
-                        Dim messages As Messages = New Messages
-                        messages.type = "text"
-                        messages.text = cDB.DRData("Message")
-                        requestmessage.add_message(messages)
+                        requestmessage.add_message(cDB.DRData("Message"))
                     End If
+
+                    'キーワード未使用
                 Else
+
+                    'メッセージ取得
                     sSQL.Clear()
                     sSQL.Append(" SELECT Message")
                     sSQL.Append(" FROM " & cCom.gctbl_MessageMst)
                     sSQL.Append(" WHERE EventID = @EventID")
                     sSQL.Append(" ORDER BY MessageID")
                     cDB.SelectSQL(sSQL.ToString)
+
+                    'メッセージリストを作成
                     Dim message_Array As New ArrayList()
                     Do Until Not cDB.ReadDr
                         message_Array.Add(cDB.DRData("Message"))
                     Loop
-                    Dim find_CouponCode_Flg As Boolean = False
+
+                    'クーポンコードの生成を一回に制限する
+                    Dim Generated_CouponCode As Boolean = False
+
+                    'クーポンコードの生成とrequestmessageへの追加
                     For Each message In message_Array
-                        If Not find_CouponCode_Flg And message.IndexOf(cCom.gcFormatCouponCode) >= 0 Then
+                        If Not Generated_CouponCode And message.IndexOf(cCom.gcFormatCouponCode) >= 0 Then
                             Dim count As Integer = 0
                             While True
                                 count += 1
+
+                                'クーポンコードの生成
                                 CouponCode = cCom.CmnGenerateAlphaNumeric(10)
                                 cDB.AddWithValue("@CouponCode" & count, CouponCode)
+
+                                'クーポンコードが使用済みか確認
                                 sSQL.Clear()
                                 sSQL.Append(" SELECT CouponCode")
                                 sSQL.Append(" FROM " & cCom.gctbl_UsedKeyword)
                                 sSQL.Append(" WHERE CouponCode = @CouponCode" & count)
                                 cDB.SelectSQL(sSQL.ToString)
+
+                                '未使用の場合Exit
                                 If Not cDB.IsSelectExistRecord() Then
                                     Exit While
                                 End If
                             End While
-                            find_CouponCode_Flg = True
+                            Generated_CouponCode = True
                         End If
+
+                        'クーポンコードに置き換え
                         message = message.Replace(cCom.gcFormatCouponCode, CouponCode)
-                        Dim messages As Messages = New Messages
-                        messages.type = "text"
-                        messages.text = message
-                        requestmessage.add_message(messages)
+                        requestmessage.add_message(message)
                     Next
+
+                    'Used_Keywordに追加
                     cDB.AddWithValue("@CouponCode", CouponCode)
                     sSQL.Clear()
                     sSQL.Append(" INSERT INTO " & cCom.gctbl_UsedKeyword)
                     sSQL.Append(" VALUES(@Line_UserID, @EventID, @ReplyToken, 0, @CouponCode, 1)")
                     cDB.ExecuteSQL(sSQL.ToString)
                 End If
+
+                'キーワード無効
             Else
+
+                'キーワード無効メッセージを取得
                 sSQL.Clear()
                 sSQL.Append(" SELECT")
                 sSQL.Append("  Message")
@@ -164,33 +216,25 @@ Partial Class API_GetContent_Index
                 sSQL.Append(" WHERE " & cCom.gctbl_EventMst & ".EventID = 0 AND MessageID = 1")
                 cDB.SelectSQL(sSQL.ToString)
                 If cDB.ReadDr Then
-                    Dim messages As Messages = New Messages
-                    messages.type = "text"
-                    messages.text = cDB.DRData("Message")
-                    requestmessage.add_message(messages)
+                    requestmessage.add_message(cDB.DRData("Message"))
                 End If
             End If
+
+            'POST送信
             Try
+
+                '仮送信ログを登録
                 sSQL.Clear()
                 sSQL.Append(" INSERT INTO " & cCom.gctbl_LogMst)
-                sSQL.Append(" VALUES(@ReplyToken, @Line_UserID, @Send, 999, 'Log', NOW())")
+                sSQL.Append(" VALUES(@ReplyToken, @Send, @Line_UserID, 999, 'Log', NOW())")
                 cDB.ExecuteSQL(sSQL.ToString)
-                'POST送信する文字列を作成
+                'POST送信するデータを作成
                 Dim postData As String = JsonConvert.SerializeObject(requestmessage)
-                Response.Write(postData)
-                'バイト型配列に変換
-                'Dim postDataBytes As Byte() = System.Text.Encoding.UTF8.GetBytes(postData)
                 req.Method = "POST"
                 req.ContentType = "application/json"
-                'POST送信するデータの長さを指定
-                'req.ContentLength = postDataBytes.Length
                 req.Headers.Add("Authorization", "Bearer " & AccessToken)
-                'データをPOST送信するためのStreamを取得
-                'Dim reqStream As System.IO.Stream = req.GetRequestStream()
-                ''送信するデータを書き込む
-                'reqStream.Write(postDataBytes, 0, postDataBytes.Length)
-                'reqStream.Close()
                 Using reqStream As New System.IO.StreamWriter(req.GetRequestStream())
+                    'POST送信
                     reqStream.Write(postData)
                 End Using
                 'サーバーからの応答を受信するためのWebResponseを取得
@@ -199,16 +243,19 @@ Partial Class API_GetContent_Index
                 Dim resStream As System.IO.Stream = res.GetResponseStream()
                 '受信して表示
                 Dim sr As New System.IO.StreamReader(resStream, enc)
-                Dim num As Integer = res.StatusCode
-                Response.Write(num)
+                Dim statuscode As Integer = res.StatusCode
                 cDB.AddWithValue("@SendLog", sr.ReadToEnd())
-                cDB.AddWithValue("@Status", num)
+                cDB.AddWithValue("@Status", statuscode)
+
+                '送信ログを更新
                 sSQL.Clear()
                 sSQL.Append(" UPDATE " & cCom.gctbl_LogMst)
                 sSQL.Append(" SET Status = @Status, Log = @SendLog")
                 sSQL.Append(" WHERE ReplyToken = @ReplyToken")
                 sSQL.Append(" AND SendRecv = @Send")
                 cDB.ExecuteSQL(sSQL.ToString)
+
+                'Reply_flgを1(送信済み)にする
                 If HasEventID_flg Then
                     sSQL.Clear()
                     sSQL.Append(" UPDATE " & cCom.gctbl_UsedKeyword)
@@ -220,19 +267,25 @@ Partial Class API_GetContent_Index
                 '閉じる
                 sr.Close()
             Catch ex As WebException
-                Response.Write(ex.Message)
-                'サーバーからの応答を受信するためのWebResponseを取得
+
+                sRet = ex.Message
+                If sRet <> "" Then
+                    cCom.CmnWriteStepLog(sRet)
+                End If
+
+                'エラーの応答を受信するためのWebResponseを取得
                 Dim res As System.Net.HttpWebResponse = ex.Response
+
                 '応答データを受信するためのStreamを取得
                 Dim resStream As System.IO.Stream = res.GetResponseStream()
+
                 '受信して表示
                 Dim sr As New System.IO.StreamReader(resStream, enc)
-                Dim num As Integer = res.StatusCode
-                'sRet = ex.Message
-                'cDB.AddWithValue("@Log", sjson & "|" & JsonConvert.SerializeObject(requestmessage))
-                Dim dumy As String = sr.ReadToEnd()
-                cDB.AddWithValue("@SendLog1", dumy)
-                cDB.AddWithValue("@Status", num)
+                Dim statuscode As Integer = res.StatusCode
+                cDB.AddWithValue("@SendLog1", sr.ReadToEnd())
+                cDB.AddWithValue("@Status", statuscode)
+
+                '送信ログ更新
                 sSQL.Clear()
                 sSQL.Append(" UPDATE " & cCom.gctbl_LogMst)
                 sSQL.Append(" SET Log = @SendLog1, Status = @Status")
@@ -242,12 +295,15 @@ Partial Class API_GetContent_Index
                 sr.Close()
             End Try
         Catch ex As Exception
+
             sRet = ex.Message
             If sRet <> "" Then
                 cCom.CmnWriteStepLog(sRet)
             End If
-            'cDB.AddWithValue("@Log", sjson & "|" & JsonConvert.SerializeObject(requestmessage))
+
             cDB.AddWithValue("@SendLog2", sRet)
+
+            '送信ログ更新
             sSQL.Clear()
             sSQL.Append(" UPDATE " & cCom.gctbl_LogMst)
             sSQL.Append(" SET Log = @SendLog2")
